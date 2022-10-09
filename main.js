ec0657183c7aa645622d1f2738d43ec97433a8aa
alexlipovka <alex.lipovka@gmail.com>
2022-10-10 00:09:16 +0200

    edit

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-import './style.css';
+import './style.css'; 
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import TileWMS from 'ol/source/TileWMS';
 import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 const map = new Map({
   target: 'map',
@@ -10,15 +11,6 @@ const map = new Map({
     new TileLayer({
         source: new OSM()
     }),
-    // new TileLayer({
-    //   source: new TileWMS({
-    //     url: 'http://localhost:8080/geoserver/wms',
-    //     params: {'LAYERS': 'tis_af18:amenity', 'TILED': true},
-    //     serverType: 'geoserver',
-    //     // Countries have transparency, so do not fade tiles:
-    //     transition: 0,  
-    //   }),
-    // }),
     new TileLayer({
       source: new TileWMS({
         url: 'http://localhost:8080/geoserver/wms',
@@ -35,11 +27,13 @@ const map = new Map({
         serverType: 'geoserver',
         // Countries have transparency, so do not fade tiles:
         transition: 0,  
-      }),
-    }),
+      }), 
+    })
   ],
   view: new View({
-    center: [10340785,7560243], 
-    zoom: 12  
+    // center: [10340785.445, 7560243], 
+    center: fromLonLat([92.852572,56.010569]),
+    zoom: 11  
   })
 });
+
