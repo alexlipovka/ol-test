e003da18ce013c5c6f3d61666198fd2ebac4aee3
alexlipovka <alex.lipovka@gmail.com>
2022-10-10 10:38:06 +0200

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,29 +1,15 @@
-@REM #!/usr/bin/env sh
+@REM Переход в папку результирущей страницы
+cd ./dist
 
-@REM # abort on errors
-@REM @REM set -e
-
-@REM # build
-npm run build
-
-@REM # navigate into the build output directory
-cd dist
-
-@REM # place .nojekyll to bypass Jekyll processing
+@REM создаем файл .nojekyll для обхода интерпретатора Jekyll Git
 echo '' > .nojekyll
-
-@REM # if you are deploying to a custom domain
-@REM # echo 'www.example.com' > CNAME
 
 git init
 git checkout -B master
 git add -A
 git commit -m 'deploy'
 
-@REM # if you are deploying to https://<USERNAME>.github.io
-@REM # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-@REM # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/alexlipovka/ol-test.git master:gh-pages
 
+@REM возвращаеемся в рабочую папку
 cd ..
