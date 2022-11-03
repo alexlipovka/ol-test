89c2a31b2d2afd7563b6371fc7bb48d64518a46c
alexlipovka <alex.lipovka@gmail.com>
2022-11-03 23:02:45 +0200

    adding ol

--- main.js
+++ main.js
@@ -11,24 +11,24 @@ const map = new Map({
     new TileLayer({
         source: new OSM()
     }),
-    // new TileLayer({
-    //   source: new TileWMS({
-    //     url: 'http://localhost:8080/geoserver/wms',
-    //     params: {'LAYERS': 'tis_af18:border', 'TILED': true },
-    //     serverType: 'geoserver',
-    //     // Countries have transparency, so do not fade tiles:
-    //     transition: 0,  
-    //   }),
-    // }),
-    // new TileLayer({
-    //   source: new TileWMS({
-    //     url: 'http://localhost:8080/geoserver/wms',
-    //     params: {'LAYERS': 'tis_af18:building', 'TILED': true, 'STYLES': 'tis_af18:black_poly' },
-    //     serverType: 'geoserver',
-    //     // Countries have transparency, so do not fade tiles:
-    //     transition: 0,  
-    //   }), 
-    // })
+    new TileLayer({
+      source: new TileWMS({
+        url: '/geoserver/wms',
+        params: {'LAYERS': 'tis_af18:border', 'TILED': true },
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,  
+      }),
+    }),
+    new TileLayer({
+      source: new TileWMS({
+        url: '/geoserver/wms',
+        params: {'LAYERS': 'tis_af18:building', 'TILED': true, 'STYLES': 'tis_af18:black_poly' },
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,  
+      }), 
+    })
   ],
   view: new View({
     // center: [10340785.445, 7560243], 
